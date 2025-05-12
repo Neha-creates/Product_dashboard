e26ca4e537abe2682835a2c2f2c0db45d1f6351a
Neha-creates <n5h1sh1rm12001@gmail.com>
2025-05-12 14:58:35 +0530

    index file update

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ const transactions = [
 let categories = transactions.map((product)=>{
     return {category: product.category, amount: product. amount}
 })
-console.log(categories);
+//console.log(categories);
+let cat1 = categories.filter((pro)=>pro.category=="Groceries");
+let cat2 = categories.filter((pro)=>pro.category=="Electronics");
+let cat3 = categories.filter((pro)=>pro.category=="Clothing");
+//console.log(cat1, cat2, cat3)
+let arr1 = [];
+for(let product of cat1){
+  arr1.push(product.amount)
+}
+arr1 = arr1.reduce((a,c)=>a+c,0);
+cat1 = {category:"Groceries", total: arr1};
+arr1 = [];
+for(let product of cat2){
+  arr1.push(product.amount)
+}
+arr1 = arr1.reduce((a,c)=>a+c,0);
+cat2 = {category:"Electronics", total: arr1};
+arr1 = [];
+for(let product of cat3){
+  arr1.push(product.amount)
+}
+arr1 = arr1.reduce((a,c)=>a+c,0);
+cat3 = {category:"Clothing", total: arr1};
+let res = [];
+res.push(cat1, cat2, cat3)
+console.log(res)
